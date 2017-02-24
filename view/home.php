597485217a4a6f26f9a8e360f676af7a94ca2304
polipetide <marta.melis@outlook.it>
2017-02-24 21:46:19 +0100

    Consegna 3

--- view/home.php
+++ view/home.php
@@ -4,7 +4,7 @@
 			
 <div> <!-- articolo-->
 	<?php				
-		$query = "SELECT * FROM Prodotto LIMIT 3";
+		$query = "SELECT * FROM Prodotto WHERE id_p = 1";
 		$result = $mysqli->query($query);
 		
 		while($row = $result->fetch_row())
